ba9745fab66ce5a3baf0534029ed0c0a637312af
ravid12 <ravid12@gmail.com>
2017-09-27 16:57:00 +1300

    initial commit

---
+++ scripts/get lyrics.py
@@ -0,0 +1,30 @@
+import string
+import time
+import re
+import urllib.request
+
+def get_lyrics(artist,song_title):
+    artist = artist.lower()
+    song_title = song_title.lower()
+    # remove all except alphanumeric characters from artist and song_title
+    artist = re.sub('[^A-Za-z0-9]+', "", artist)
+    song_title = re.sub('[^A-Za-z0-9]+', "", song_title)
+    if artist.startswith("the"):    # remove starting 'the' from artist e.g. the fratellis -> fratellis (thats how AZ lyrics works)
+        artist = artist[3:]
+    url = "http://azlyrics.com/lyrics/"+artist+"/"+song_title+".html"
+    
+    try:
+        content = urllib.request.urlopen(url).read()
+#        soup = BeautifulSoup(content, 'html.parser')
+#        lyrics = str(soup)
+        lyrics = str(content)    
+        # lyrics lies between start and end
+        start = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
+        end = '<!-- MxM banner -->'
+        lyrics = (lyrics.split(start))[1].split(end)[0]
+        lyrics = lyrics.replace('<br>','').replace('</br>','').replace('</div>','').strip()
+        return lyrics
+    except Exception as e:
+        return "Exception occurred \n" +str(e)
+
+print (get_lyrics("green day", "bouncing off the wall"))
