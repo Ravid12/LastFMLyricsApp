6a64f0f354791c867e2cb4df6c80ec4655e5adf9
Ravid12 <ravid12@gmail.com>
2017-10-09 00:06:34 +1300

    finished app (i think)

--- webpage/main.js
+++ webpage/main.js
@@ -1,10 +1,17 @@
 const api_key = 'fcdddfcc2be3b398be4e1c3dc6ec3ba7'
 var username;
 
+var songname;
+var songartist;
+
 
 function setUser(username) {
-    this.username = username;
-    getCurrentlyPlaying();
+    if (username == undefined){
+        alert("please enter a username");
+    } else {
+        this.username = username;
+        getCurrentlyPlaying();
+    }
 }
 
 function getCurrentlyPlaying() {
@@ -13,14 +20,31 @@ function getCurrentlyPlaying() {
         dataType: 'json',
         async: false,
         success: function (data) {
-            // Go through each song
-            $.each(data.recenttracks.track, function (i, song) {
-                if (i==0) {
-                    console.log(song.artist['#text']);
-                    console.log(song.name);
-                }
-            });
+            if (data.recenttracks.track.length == 1){
+                console.log("not currently playing anything");
+            } else {
+                // Go through each song
+                $.each(data.recenttracks.track, function (i, song) {
+                    // save details of first song (currently playing)
+                    if (i==0) {
+                        songartist=(song.artist['#text']);
+                        songname=(song.name);
+                    }
+                });
+
+                songartist = songartist.replace(/[^a-z0-9]/gi,'').toLowerCase();
+                songname = songname.replace(/[^a-z0-9]/gi,'').toLowerCase();
+
+                let url = "http://azlyrics.com/lyrics/"+songartist+"/"+songname+".html"                
+                // have to host python on the server somehow?
+                $.ajax({ url: url, success: function(data) { 
+                    alert(data); 
+                } });
+                
+
+            }
         }
     });
-    
+    console.log(songname);
+    console.log(songartist);
 }
